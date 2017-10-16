c281f0e2080c1a68f762a8b42b0cc067381649e1
xuexing <xuexing@time-stone.com>
2017-10-16 09:43:31 +0800

    pull

--- main/run.js
+++ main/run.js
@@ -4,10 +4,10 @@ var model = {};
 const opts = {url: "http://www.time-stone.cn"}
 http_send(opts,model); //需要判断一个状态 检测send结束 有了 html数据
 
-// setInterval(function () {
-//     console.log("- begin -");
-//     console.log(model.$html("title").text());
-// },4000);
+setInterval(function () {
+    console.log("- begin -");
+    console.log(model.$html("title").text());
+},4000);
 
 function CANDY() {
     this.model = {a:1};
